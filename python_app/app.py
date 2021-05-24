5d36f300f1b9279c1cecc366b1e6d6fe80d090f1
raghavendersridhar <raghav.ajay3@gmail.com>
2021-05-24 18:31:57 +0530

    added swagger

--- python_app/app.py
+++ python_app/app.py
@@ -5,48 +5,39 @@ import re
 from flask import Flask, request, jsonify
 from postal.parser import parse_address
 from postal.expand import expand_address
+from flasgger import Swagger
 
 app =Flask(__name__)
-# api =API(app)
+swagger = Swagger(app)
 
-
-# class Address:
-
-#     def __init__(self):
-#         self.result = []
-#         self.final_parser_address = {}
-
-#     def parserv3(self, address):
-#         address = re.sub(r"\([^)]*\)","", address)
-#         result = parse_address(address)
-#         return result
-
-# class Parserv3(Resource):
-#     def get(self, address):
-#         add_parser = urllib.parse.unquote(address)
-#         result = add_parser.parserv3(address)
-#         return jsonify(result)
-
-# api.add_resource(Parserv3,"/addressParserv3/<address>")
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0",port ="8080")
-
-# app = Flask(__name__)
- 
-# classifier = TextClassifier.load('en-sentiment')
 @app.route('/')
 def hello():
     return 'Hello, World!'
  
 @app.route('/api/v1/addressparser', methods=['POST'])
 def addressparser():
-    text = request.form['address']
-    text = str(text)
-    result=parse_address(text)
-    response = {'result':result}
-    return jsonify(response), 200
- 
+    """Example endpoint returning details of address parser
+    ---
+    parameters:
+      - name: address
+        in: query
+        type: string
+        required: true
+      
+    responses:
+      200:
+        description: the address parser returns all the sub details 
+    """
+    try:
+        # text = request.form['address']
+        address_value = str(request.args.get("address"))
+        # text = str(text)
+        result=parse_address(address_value)
+        response = {'result':result}
+        return jsonify(response)
+    except Exception as e:
+        print(e)
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port ="8080")
 
